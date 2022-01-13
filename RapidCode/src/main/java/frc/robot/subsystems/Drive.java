e83f37386eda0ee39cfbaa79fefafd3baf384a49
rhit-edgingza <edgingza@rose-hulman.edu>
2022-01-12 20:34:29 -0500

    Created the subsystems folder and the drive class

---
+++ RapidCode/src/main/java/frc/robot/subsystems/Drive.java
@@ -0,0 +1,15 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Drive extends SubsystemBase {
+    public Drive() { 
+        
+    }
+
+    @Override
+    public void periodic() {
+        
+    }
+}
