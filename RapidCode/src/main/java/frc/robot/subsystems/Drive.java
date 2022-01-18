3c3abe3fa15ffd9ceffef7b94cb019d77b71b09a
rhit-edgingza <edgingza@rose-hulman.edu>
2022-01-18 10:53:25 -0500

    Added detailed comments to Drive

--- RapidCode/src/main/java/frc/robot/subsystems/Drive.java
+++ RapidCode/src/main/java/frc/robot/subsystems/Drive.java
@@ -11,31 +11,47 @@ public class Drive extends SubsystemBase {
     private double linearPower;
 
     public Drive() { 
-        
+        /*
+        Initialize all objects and variables
+        Objects are things like motor controllers, solenoids, and sensors
+        Variables are things like important numbers/constants to keep track of
+
+        To initialize an object, use general format: ObjectType objectName = new ObjectType(initParameters)
+        If you're giving a port number in the object parameters, reference the Constants class: Constants.PortType.STATIC_VALUE
+        */
     }
 
     @Override
     public void periodic() {
-        
+        /*
+        A method that loops every 20ms
+        Use this to update important values
+        Try and avoid using this method; rather, use a command
+        Ask mentors before putting things in here!
+        */
     }
 
     private void driveRaw(double left, double right) {
 	    /*
-	       Percent output takes a "speed" from -1 to 1.
-	       Follow tells a motor controller to match the speed of another.
-	     */
+	    This method is used to supply values to the drive motors
+        Variables left and right will be between -1 and 1
+        Call the .set(left) for the primary drive motor on the left
+        Call the .set(right) for the primary drive motor on the right
+	    */
 	}
 
-    public void cheesyDrive(double throttle, double wheel, double quickTurn, boolean shifter) {
-        /*This code belongs to the poofs*/
+    public void cheesyDrive(double throttle, double wheel, double quickTurn, boolean shifted) {
+        /*
+        This code belongs to the poofs
+        Please don't touch this code, but feel free to read and ask questions
+        */
 
         negInertia = wheel - oldWheel;
         oldWheel = wheel;
 
-        wheelNonLinearity = 0.5;
-        //wheelNonLinearity = shifter ? 0.6:0.5;
+        wheelNonLinearity = (shifted) ? 0.6 : 0.5;
 
-        /*Apply a sin function that's scaled to make it feel better.*/
+        /*Apply a sine function that's scaled to make it feel better.*/
         wheel = Math.sin(Math.PI / 2.0 * wheelNonLinearity * wheel)
                 / Math.sin(Math.PI / 2.0 * wheelNonLinearity);
         wheel = Math.sin(Math.PI / 2.0 * wheelNonLinearity * wheel)
@@ -43,11 +59,7 @@ public class Drive extends SubsystemBase {
         wheel = Math.sin(Math.PI / 2.0 * wheelNonLinearity * wheel)
                 / Math.sin(Math.PI / 2.0 * wheelNonLinearity);
 
-        if(shifter){
-            sensitivity = .5;
-        } else {
-            sensitivity = 1;
-        }
+        sensitivity = (shifted) ? 0.5 : 1.0;
 
         wheel += negInertia;
         linearPower = throttle;
@@ -62,4 +74,31 @@ public class Drive extends SubsystemBase {
         double right = (linearPower - angularPower) * sensitivity;
         driveRaw(left, right);
     }
+
+    public void driveArcade(double throttle, double turn, boolean shifter) {
+        /*Please don't touch this code, but feel free to read and ask questions*/
+
+	    /*shifter multiplier*/
+		double shiftVal = shifter ? 1 : 0.5;
+
+		double lDrive;
+        double rDrive;
+
+        /*reverse turning when driving backwards*/
+		if (-throttle < 0){
+			turn = turn * -1;
+		}
+
+		/*if there is no throttle do a zero point turn, or a "quick turn"*/
+		if (Math.abs(throttle) < 0.05) {
+			lDrive = -turn * shiftVal * 0.75;
+			rDrive = turn * shiftVal * 0.75;
+		} else {
+			/*if not driving with quick turn then driveTrain with split arcade*/
+			lDrive = shiftVal * throttle * (1 + Math.min(0, turn));
+			rDrive = shiftVal * throttle * (1 - Math.max(0, turn));
+		}
+
+		driveRaw(lDrive, rDrive);
+	}
 }
