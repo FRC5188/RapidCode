0bede7085a10630cead6d665c3004d479f89b1c9
rhit-edgingza <edgingza@rose-hulman.edu>
2022-02-01 20:19:23 -0500

    Added commands to pickup

---
+++ RapidCode/src/main/java/frc/robot/commands/CmdPickupStow.java
@@ -0,0 +1,25 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class CmdPickupStow extends CommandBase {
+    public CmdPickupStow() {
+    }
+
+    @Override
+    public void initialize() {
+    }
+
+    @Override
+    public void execute() {
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
