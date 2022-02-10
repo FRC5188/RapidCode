3696c5d1bbf2eebcd8e608909be9ab4f36302d72
Ahan Bhattacharyya <ahanrocks123@gmail.com>
2022-02-09 19:31:52 -0500

    Added shooter motors and set up shoot command

--- RapidCode/src/main/java/frc/robot/subsystems/Shooter.java
+++ RapidCode/src/main/java/frc/robot/subsystems/Shooter.java
@@ -4,14 +4,32 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.InvertType;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+// Current Goal: full send motors when button pressed
 
 public class Shooter extends SubsystemBase {
-  /** Creates a new Shooter. */
-  public Shooter() {}
+    //adding motor controllers for falcon
+    private WPI_TalonFX m_shooterLeft;
+    private WPI_TalonFX m_shooterRight;
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+    public Shooter() {
+        m_shooterLeft = new WPI_TalonFX(Constants.CAN.LEFT_SHOOTER_ID);
+        m_shooterRight = new WPI_TalonFX(Constants.CAN.RIGHT_SHOOTER_ID);
+
+        m_shooterRight.setInverted(InvertType.InvertMotorOutput);
+    }
+
+    @Override
+    public void periodic() {
+    }
+
+    public void shoot() {
+        m_shooterLeft.set(1);
+        m_shooterRight.set(1);
+    }
 }
