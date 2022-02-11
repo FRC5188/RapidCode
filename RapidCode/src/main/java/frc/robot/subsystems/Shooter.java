0a3bfeadd6cc52e4109c4e32e9b8803c7c7ca0e3
Ahan Bhattacharyya <ahanrocks123@gmail.com>
2022-02-10 20:08:31 -0500

    Made hood PID + hood/turret motors/potentiometers

--- RapidCode/src/main/java/frc/robot/subsystems/Shooter.java
+++ RapidCode/src/main/java/frc/robot/subsystems/Shooter.java
@@ -6,39 +6,72 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.InvertType;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 // Current Goal: full send motors when button pressed
 
 public class Shooter extends SubsystemBase {
-    //adding motor controllers for falcon
-    private WPI_TalonFX m_shooterLeft;
-    private WPI_TalonFX m_shooterRight;
+    //adding motor controllers for falcon shooter, sparkmax hood/turret, analog hood/turret potentiometers, and hood PID
+    private WPI_TalonFX m_flywheelLeft;
+    private WPI_TalonFX m_flywheelRight;
+
+    private CANSparkMax m_hood;
+    private CANSparkMax m_turret;
+
+    private AnalogInput m_hoodPotentiometer;
+    private AnalogInput m_turretPotentiometer;
+
+    private PIDController m_hoodPID;
+
 
     public Shooter() {
-        m_shooterLeft = new WPI_TalonFX(Constants.CAN.LEFT_SHOOTER_ID);
-        m_shooterRight = new WPI_TalonFX(Constants.CAN.RIGHT_SHOOTER_ID);
+        m_flywheelLeft = new WPI_TalonFX(Constants.CAN.LEFT_FLYWHEEL_ID);
+        m_flywheelRight = new WPI_TalonFX(Constants.CAN.RIGHT_FLYWHEEL_ID);
 
-        m_shooterRight.setInverted(InvertType.InvertMotorOutput);
+        m_flywheelRight.setInverted(InvertType.InvertMotorOutput);
+
+        m_hood = new CANSparkMax(Constants.CAN.HOOD_MOTOR_ID, MotorType.kBrushless);
+
+        m_turret = new CANSparkMax(Constants.CAN.TURRET_MOTOR_ID, MotorType.kBrushless);
+
+        m_hoodPotentiometer = new AnalogInput(Constants.AIO.HOOD_POTENTIOMETER_PORT);
+        m_turretPotentiometer = new AnalogInput(Constants.AIO.TURRET_POTENTIOMETER_PORT);
+
+        m_hoodPID = new PIDController(0.1, 0, 0);
+
     }
 
     @Override
     public void periodic() {
     }
 
-    public void setShooterSpeed(double speed){
-        m_shooterLeft.set(speed);
-        m_shooterRight.set(speed);
+    public void setFlywheelSpeed(double speed){
+        m_flywheelLeft.set(speed);
+        m_flywheelRight.set(speed);
     }
 
-    public double getShooterSpeedSetpoint() {
-        return m_shooterRight.get() * 6000/*max RPM*/;
+    public double getFlywheelSpeedSetpoint() {
+        return m_flywheelRight.get() * 6000/*max RPM*/;
     }
 
-    public double getShooterRPM(){
+    public double getFlywheelRPM(){
         //units per 100ms, 2048 units per rotation
-        return m_shooterRight.getSelectedSensorVelocity() * 600 * (1/2048.0);
+        return m_flywheelRight.getSelectedSensorVelocity() * 600 * (1/2048.0);
     }   
+
+    public double getHoodPotentiometerAngle() {
+        return m_hoodPotentiometer.getVoltage(); //needs to be converted to angle
+    }
+
+    public double getTurretPotentiometerAngle() {
+        return m_turretPotentiometer.getVoltage(); //needs to be converted to angle
+    }
+    
+
 }
