cb4f68180ab9fe4d4ff241e1b6f40857a58dc050
rhit-edgingza <edgingza@rose-hulman.edu>
2022-02-17 10:18:57 -0500

    Cleaned up command and commented changes

--- RapidCode/src/main/java/frc/robot/commands/CmdShooterShoot.java
+++ RapidCode/src/main/java/frc/robot/commands/CmdShooterShoot.java
@@ -1,26 +1,26 @@
 package frc.robot.commands;
 
-import java.util.function.DoubleSupplier;
-
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Shooter;
 
 public class CmdShooterShoot extends CommandBase {
     Shooter m_shooterSubsystem;
+    // Should not need to use a DoubleSupplier; just a double is good
+    // Also the turret control will probably be a separate command, but we want speed to be variable, so I added that to parameters
+    double m_hoodAngle;
+    double m_speed;
 
-    DoubleSupplier m_hoodAngle;
-    DoubleSupplier m_turretAngle;
-
-    public CmdShooterShoot(Shooter shooterSubsystem) {
+    public CmdShooterShoot(Shooter shooterSubsystem, double hoodAngle, double speed) {
         m_shooterSubsystem = shooterSubsystem;
         m_hoodAngle = hoodAngle;
-        m_turretAngle = turretAngle;
-
-        addRequirements(shooterSubsystem);
+        m_speed = speed;
+        // Don't need to require subsystem; only do that if it is the default command
     }
 
     @Override
     public void initialize() {
+        //only set the hood setpoint once, so in init, not execute
+        m_shooterSubsystem.setHoodSetPoint(m_hoodAngle);
     }
 
     @Override
@@ -31,20 +31,23 @@ public class CmdShooterShoot extends CommandBase {
         if (m_shooterSubsystem.atHoodSetpoint() && (m_shooterSubsystem.getFlywheelRPM() >= m_shooterSubsystem.getFlywheelSpeedSetpoint())) {
             //shoot :)
         } else {
-            m_shooterSubsystem.setHoodSetPoint(m_hoodAngle.getAsDouble());
             m_shooterSubsystem.hoodPIDExecute();
-            m_shooterSubsystem.setTopFlywheelSpeed(1);
-            m_shooterSubsystem.setBottomFlywheelSpeed(1);
+            m_shooterSubsystem.setTopFlywheelSpeed(m_speed);
+            m_shooterSubsystem.setBottomFlywheelSpeed(m_speed);
         }
         
     }
 
     @Override
     public void end(boolean interrupted) {
+        // Should set flywheel speed to 0 when the command ends
+        m_shooterSubsystem.setTopFlywheelSpeed(0);
+        m_shooterSubsystem.setBottomFlywheelSpeed(0);
     }
 
     @Override
     public boolean isFinished() {
+        // change after merge to finish when ball count is 0
         return false;
     }
 }
