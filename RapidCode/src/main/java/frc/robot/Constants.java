0f718d60f6d0b06e4463bc2c6ac721062606a37a
rhit-edgingza <edgingza@rose-hulman.edu>
2022-02-17 10:39:49 -0500

    Changes for shooter subsystem. Read comments

--- RapidCode/src/main/java/frc/robot/Constants.java
+++ RapidCode/src/main/java/frc/robot/Constants.java
@@ -2,11 +2,13 @@ package frc.robot;
 
 public final class Constants {
     /*
-    The class that holds constant values
-    All names should be all caps with _ between to denote a static (unchangeable) value
-    If it is a general constant (not a port/id), put between this comment and the next class
-    If it is a port/id, put it into its class based on type (CAN, DIO, AIO, etc)
-    */
+     * The class that holds constant values
+     * All names should be all caps with _ between to denote a static (unchangeable)
+     * value
+     * If it is a general constant (not a port/id), put between this comment and the
+     * next class
+     * If it is a port/id, put it into its class based on type (CAN, DIO, AIO, etc)
+     */
     public static final double QUICK_TURN_DEADBAND = 0.05;
     public static final double QUICK_TURN_MULTIPLIER = 0.75;
     public static final double ARCADE_DRIVE_DEADBAND = 0.025;
@@ -15,11 +17,12 @@ public final class Constants {
 
     public final class AIO {
         /*
-        The class that holds the AIO ports used (AIO = analog input/output)
-        Used for analog sensors: they return a range of values
-        Sensor examples include potentiometers, ultrasonic sensors, range sensors
-        Name example: public static final int TURRET_POT_PORT = 0 (name what sensor is/does and end with port)
-        */
+         * The class that holds the AIO ports used (AIO = analog input/output)
+         * Used for analog sensors: they return a range of values
+         * Sensor examples include potentiometers, ultrasonic sensors, range sensors
+         * Name example: public static final int TURRET_POT_PORT = 0 (name what sensor
+         * is/does and end with port)
+         */
         public static final int HOOD_POTENTIOMETER_PORT = 0;
         public static final int TURRET_POTENTIOMETER_PORT = 1;
 
@@ -27,57 +30,62 @@ public final class Constants {
 
     public final class CAN {
         /*
-        The class that holds the CAN ids for each motor
-        Most of the motors/motor controllers are labelled with a number, so use that as ID
-        Do not use an id of 0; this is a default for things like the PDH, PCM, and roboRIO, so start at 1
-        Name example: public static final int LEFT_PRIMARY_DRIVE_ID (name with what motor does and end with id)
-        */
+         * The class that holds the CAN ids for each motor
+         * Most of the motors/motor controllers are labelled with a number, so use that
+         * as ID
+         * Do not use an id of 0; this is a default for things like the PDH, PCM, and
+         * roboRIO, so start at 1
+         * Name example: public static final int LEFT_PRIMARY_DRIVE_ID (name with what
+         * motor does and end with id)
+         */
         public static final int LEFT_PRIMARY_DRIVE_ID = 1;
         public static final int LEFT_SECONDARY_DRIVE_ID = 2;
         public static final int RIGHT_PRIMARY_DRIVE_ID = 3;
         public static final int RIGHT_SECONDARY_DRIVE_ID = 4;
-        public static final int LEFT_FLYWHEEL_ID = 5;
-        public static final int RIGHT_FLYWHEEL_ID = 6;
-        public static final int HOOD_MOTOR_ID = 7;
-        public static final int TURRET_MOTOR_ID = 8;
-
-
+        // 5 and 6 are already used in the ball path, so make sure to not duplicate
+        public static final int LEFT_FLYWHEEL_ID = 7;
+        public static final int RIGHT_FLYWHEEL_ID = 8;
+        public static final int HOOD_MOTOR_ID = 9;
+        public static final int TURRET_MOTOR_ID = 10;
 
     }
 
     public final class DIO {
         /*
-        The class that holds the DIO ports used (DIO = digital input/output)
-        Used for digital types of sensors: they only return a true or false
-        Sensor examples include some encoders, photoelectric sensors
-        Name example: public static final int PICKUP_SENSOR_PORT = 0 (name what sensor is/does and end with port)
-        */
+         * The class that holds the DIO ports used (DIO = digital input/output)
+         * Used for digital types of sensors: they only return a true or false
+         * Sensor examples include some encoders, photoelectric sensors
+         * Name example: public static final int PICKUP_SENSOR_PORT = 0 (name what
+         * sensor is/does and end with port)
+         */
     }
 
     public final class PCM {
         /*
-        The class that holds the solenoids used on the robot
-        The PCM (Pneumatics Control Module) controls all the solenoids and is connected through CAN
-        Will only put solenoid ports into this class
-        Name example: public static final int PICKUP_SOLENOID = 0 (name what solenoid does and end with solenoid)
-        */
+         * The class that holds the solenoids used on the robot
+         * The PCM (Pneumatics Control Module) controls all the solenoids and is
+         * connected through CAN
+         * Will only put solenoid ports into this class
+         * Name example: public static final int PICKUP_SOLENOID = 0 (name what solenoid
+         * does and end with solenoid)
+         */
     }
-    
+
     public final class ButtonMappings {
         /*
-        The class that holds the button mappings for an X-box controller
-        Reference the all-caps name in code to use the buttons
-        You should not need to add anything to this class, so don't touch please!
-        */
-        public static final int A_BUTTON         = 1;
-        public static final int B_BUTTON         = 2;
-        public static final int X_BUTTON         = 3;
-        public static final int Y_BUTTON         = 4;
-        public static final int LEFT_BUMPER      = 5;
-        public static final int RIGHT_BUMPER     = 6;
-        public static final int BACK_BUTTON      = 7;
-        public static final int START_BUTTON     = 8;
-        public static final int LEFT_JOY_BUTTON  = 9;
+         * The class that holds the button mappings for an X-box controller
+         * Reference the all-caps name in code to use the buttons
+         * You should not need to add anything to this class, so don't touch please!
+         */
+        public static final int A_BUTTON = 1;
+        public static final int B_BUTTON = 2;
+        public static final int X_BUTTON = 3;
+        public static final int Y_BUTTON = 4;
+        public static final int LEFT_BUMPER = 5;
+        public static final int RIGHT_BUMPER = 6;
+        public static final int BACK_BUTTON = 7;
+        public static final int START_BUTTON = 8;
+        public static final int LEFT_JOY_BUTTON = 9;
         public static final int RIGHT_JOY_BUTTON = 10;
     }
 }
--- RapidCode/src/main/java/frc/robot/subsystems/Shooter.java
+++ RapidCode/src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,8 +1,10 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.InvertType;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.math.controller.PIDController;
@@ -14,27 +16,33 @@ import frc.robot.Constants;
 public class Shooter extends SubsystemBase {
     private WPI_TalonFX m_flywheelTop;
     private WPI_TalonFX m_flywheelBottom;
-
-    private CANSparkMax m_hood;
-    private CANSparkMax m_turret;
+    // Describe your objects better, aka don't call it hood, I don't know if its a motor or a sensor
+    private CANSparkMax m_hoodMotor;
+    private CANSparkMax m_turretMotor;
 
     private AnalogInput m_hoodPotentiometer;
     private AnalogInput m_turretPotentiometer;
 
     private PIDController m_hoodPID;
     private PIDController m_turretPID;
-    double m_hoodSetpoint = 0;
+    // Make sure to add in the scope of the thing you declare here (private or public)
+    // Preferably, don't give anything a value in here until the constructor. Only constants should get a value up here
+    private double m_hoodSetpoint;
+    private double m_turretSetpoint;
 
 
     public Shooter() {
         m_flywheelTop = new WPI_TalonFX(Constants.CAN.LEFT_FLYWHEEL_ID);
         m_flywheelBottom = new WPI_TalonFX(Constants.CAN.RIGHT_FLYWHEEL_ID);
-
+        m_flywheelTop.setNeutralMode(NeutralMode.Coast);
+        m_flywheelBottom.setNeutralMode(NeutralMode.Coast);
         m_flywheelBottom.setInverted(InvertType.InvertMotorOutput);
 
-        m_hood = new CANSparkMax(Constants.CAN.HOOD_MOTOR_ID, MotorType.kBrushless);
+        m_hoodMotor = new CANSparkMax(Constants.CAN.HOOD_MOTOR_ID, MotorType.kBrushless);
+        m_hoodMotor.setIdleMode(IdleMode.kBrake);
 
-        m_turret = new CANSparkMax(Constants.CAN.TURRET_MOTOR_ID, MotorType.kBrushless);
+        m_turretMotor = new CANSparkMax(Constants.CAN.TURRET_MOTOR_ID, MotorType.kBrushless);
+        m_turretMotor.setIdleMode(IdleMode.kBrake);
 
         m_hoodPotentiometer = new AnalogInput(Constants.AIO.HOOD_POTENTIOMETER_PORT);
         m_hoodPotentiometer.setAverageBits(2);
@@ -45,10 +53,13 @@ public class Shooter extends SubsystemBase {
         m_turretPotentiometer.setOversampleBits(0);
 
         m_hoodPID = new PIDController(0.1, 0, 0);
-        m_turretPID = new PIDController(0.1, 0, 0);
-
         m_hoodPID.setTolerance(1);
+
+        m_turretPID = new PIDController(0.1, 0, 0);
         m_turretPID.setTolerance(1);
+
+        m_hoodSetpoint = 0;
+        m_turretSetpoint = 0;
     }
 
     @Override
@@ -73,11 +84,11 @@ public class Shooter extends SubsystemBase {
     }
 
     public void setHoodSpeed(double speed){
-        m_hood.set(speed);
+        m_hoodMotor.set(speed);
     }
 
     public void hoodPIDExecute() {
-        m_hood.set(m_hoodPID.calculate(m_hoodPotentiometer.getAverageValue()));
+        m_hoodMotor.set(m_hoodPID.calculate(m_hoodPotentiometer.getAverageValue()));
     }
 
     public void setHoodSetPoint(double setpoint) {
@@ -86,7 +97,9 @@ public class Shooter extends SubsystemBase {
     }
 
     public double getHoodSetPoint() {
-        return m_hoodPID.getSetpoint();
+        // Remember we have a variable to keep track of this rather than calling a method
+        // Calling a method takes much longer than calling a variable, so always opt to use a stored value than to call it for a get
+        return m_hoodSetpoint;
     }
 
     public boolean atHoodSetpoint() {
@@ -102,19 +115,20 @@ public class Shooter extends SubsystemBase {
     }    
 
     public void setTurretSpeed(double speed) {
-        m_turret.set(speed);
+        m_turretMotor.set(speed);
     }
 
     public void setTurretSetpoint(double setpoint){
+        m_turretSetpoint = setpoint;
         m_turretPID.setSetpoint(setpoint);
     }
 
     public double getTurretSetpoint() {
-        return m_turretPID.getSetpoint();
+        return m_turretSetpoint;
     }
 
     public void turretPIDExecute() {
-        m_turret.set(m_turretPID.calculate(m_turretPotentiometer.getAverageValue()));
+        m_turretMotor.set(m_turretPID.calculate(m_turretPotentiometer.getAverageValue()));
     }
 
     public boolean atTurretSetpoint() {
