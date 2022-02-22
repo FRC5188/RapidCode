93b7c27f9c4c29c3ee154b3b1bc9773dfd69ea4c
rhit-edgingza <edgingza@rose-hulman.edu>
2022-02-22 18:05:07 -0500

    Added auto comments

--- RapidCode/src/main/java/frc/robot/commands/GrpAutoExample.java
+++ RapidCode/src/main/java/frc/robot/commands/GrpAutoExample.java
@@ -5,9 +5,12 @@ import frc.robot.subsystems.Drive;
 import frc.robot.subsystems.Pickup;
 
 public class GrpAutoExample extends SequentialCommandGroup {
+    // put any subsystems used in the auto routine in the parameters
+    // you don't need any member level variables
     public GrpAutoExample(Drive driveSubsystem, Pickup pickupSubsystem) {
 
         addCommands(
+            // add a command here. The order you put them in is the order they will run
             new CmdDriveDistance(driveSubsystem, 12, true),
             new CmdDriveRotate(driveSubsystem, 90, true),
             new CmdPickupDeploy(pickupSubsystem),
