be82151b4483dd2518f20dba9b6b946207037207
mitchgres <mgresham@ivytech.edu>
2022-02-22 20:06:43 -0500

    Made our first auto routine and commented it.

--- RapidCode/src/main/java/frc/robot/commands/GrpAutoExample.java
+++ RapidCode/src/main/java/frc/robot/commands/GrpAutoExample.java
@@ -8,13 +8,28 @@ public class GrpAutoExample extends SequentialCommandGroup {
     // put any subsystems used in the auto routine in the parameters
     // you don't need any member level variables
     public GrpAutoExample(Drive driveSubsystem, Pickup pickupSubsystem) {
-
+        /**
+         * Autonomous Mode Procedure Version 1
+         * Purpose: It will drive from the tarmac, pick up one ball, and shoot two balls into the high goal.
+         * Result: We'll hopefully get ten points, we can get a RP if our other alliance members are able to score three more cargo in AUTO. 
+         */
         addCommands(
-            // add a command here. The order you put them in is the order they will run
-            new CmdDriveDistance(driveSubsystem, 12, true),
-            new CmdDriveRotate(driveSubsystem, 90, true),
-            new CmdPickupDeploy(pickupSubsystem),
+            /* 
+            This purpose of this section of code is assuming that the robot is faced away from the hub in the tarmac, and also that
+            the technician has place the robot where it is a straight path to the ball. 
+            
+            The section will leave the tarmac, pick up one ball, and then shoot the one which is stored and also the one which it's just
+            picked up. 
+            */
+            new CmdPickupDeploy(pickupSubsystem), // Engages the grabber at the front of the robot. 
+            new CmdDriveDistance(driveSubsystem, 99, true), /* Drives 99 Inches torward the ball, we plan on adding code which will check if a ball
+            has been passed in. */
+            new CmdDriveRotate(driveSubsystem, 180, true), // Turns the robot 180 degrees. 
             new CmdPickupStow(pickupSubsystem)
+            /* 
+            Add shooter statement later, we'll need the speed and then the hoodAngle for the shooter method, which will be
+            given by the table which we'll also make later.
+             */
         );
     }
 }
