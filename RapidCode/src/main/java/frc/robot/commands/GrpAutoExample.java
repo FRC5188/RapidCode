15cd2781ee6188b4a797eb5dff92b385fc89fd73
Devin Childress <devin.childress08@gmail.com>
2022-02-22 19:42:34 -0500

    set values for auto group

--- RapidCode/src/main/java/frc/robot/commands/GrpAutoExample.java
+++ RapidCode/src/main/java/frc/robot/commands/GrpAutoExample.java
@@ -3,6 +3,7 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.Drive;
 import frc.robot.subsystems.Pickup;
+import frc.robot.subsystems.Vision;
 
 public class GrpAutoExample extends SequentialCommandGroup {
     // put any subsystems used in the auto routine in the parameters
@@ -11,9 +12,9 @@ public class GrpAutoExample extends SequentialCommandGroup {
 
         addCommands(
             // add a command here. The order you put them in is the order they will run
-            new CmdDriveDistance(driveSubsystem, 12, true),
-            new CmdDriveRotate(driveSubsystem, 90, true),
             new CmdPickupDeploy(pickupSubsystem),
+            new CmdDriveDistance(driveSubsystem, 99, true), //values are in inches
+            new CmdDriveRotate(driveSubsystem, 180, true),
             new CmdPickupStow(pickupSubsystem)
         );
     }
