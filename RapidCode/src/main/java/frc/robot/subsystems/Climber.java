1f39ad820cad19ffa9ded08e9513fa03c2898a81
first5188 <first5188@gmail.com>
2022-03-09 18:41:51 -0500

    made climber formating consistent

--- RapidCode/src/main/java/frc/robot/subsystems/Climber.java
+++ RapidCode/src/main/java/frc/robot/subsystems/Climber.java
@@ -3,43 +3,48 @@ package frc.robot.subsystems;
 import frc.robot.Constants;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Climber extends SubsystemBase {
-  private final TalonFX climberRight = new TalonFX(Constants.CAN.CLIMBER_MOTOR_ID_RIGHT); 
-  private final TalonFX climberLeft = new TalonFX(Constants.CAN.CLIMBER_MOTOR_ID_LEFT); 
-  /** Creates a new climberSubsystem. */
-  public Climber() {}
+    private WPI_TalonFX m_climberRight;
+    private WPI_TalonFX m_climberLeft;
 
-  @Override
-  public void periodic() {
-
-  }
-
-  public void climberUp () {
-    this.climberRight.set(ControlMode.PercentOutput, -Constants.CLIMBER_SPEED);
-    this.climberLeft.set(ControlMode.PercentOutput, -Constants.CLIMBER_SPEED);
-  }
-
-  public void climberDown () {
-    this.climberRight.set(ControlMode.PercentOutput, Constants.CLIMBER_SPEED);
-    this.climberLeft.set(ControlMode.PercentOutput, Constants.CLIMBER_SPEED);
-  }
-  //checks how much the trigger is pressed down, .2 being 20%, that way we dont accidentally start the climber
-  public void climberMove(Double upSpeed, Double downSpeed) {
-    if(upSpeed < -.2) {
-      this.climberRight.set(ControlMode.PercentOutput, -upSpeed);
-      this.climberLeft.set(ControlMode.PercentOutput, -upSpeed);
-    } else if (downSpeed < -.2) {
-      this.climberRight.set(ControlMode.PercentOutput, downSpeed);
-      this.climberLeft.set(ControlMode.PercentOutput, downSpeed);
+    /** Creates a new climberSubsystem. */
+    public Climber() {
+        m_climberRight = new WPI_TalonFX(Constants.CAN.CLIMBER_MOTOR_ID_RIGHT);
+        m_climberLeft = new WPI_TalonFX(Constants.CAN.CLIMBER_MOTOR_ID_LEFT);
     }
-  }
 
-  public void stop() {
-    this.climberRight.set(ControlMode.PercentOutput, 0);
-    this.climberLeft.set(ControlMode.PercentOutput, 0);
-  }
+    @Override
+    public void periodic() {
+    }
+
+    public void climberUp() {
+        m_climberRight.set(ControlMode.PercentOutput, -Constants.CLIMBER_SPEED);
+        m_climberLeft.set(ControlMode.PercentOutput, -Constants.CLIMBER_SPEED);
+    }
+
+    public void climberDown() {
+        m_climberRight.set(ControlMode.PercentOutput, Constants.CLIMBER_SPEED);
+        m_climberLeft.set(ControlMode.PercentOutput, Constants.CLIMBER_SPEED);
+    }
+
+    // checks how much the trigger is pressed down, .2 being 20%, that way we dont
+    // accidentally start the climber
+    public void climberMove(Double upSpeed, Double downSpeed) {
+        if (upSpeed < -.2) {
+            m_climberRight.set(ControlMode.PercentOutput, -upSpeed);
+            m_climberLeft.set(ControlMode.PercentOutput, -upSpeed);
+        } else if (downSpeed < -.2) {
+            m_climberRight.set(ControlMode.PercentOutput, downSpeed);
+            m_climberLeft.set(ControlMode.PercentOutput, downSpeed);
+        }
+    }
+
+    public void stop() {
+        m_climberRight.set(ControlMode.PercentOutput, 0);
+        m_climberLeft.set(ControlMode.PercentOutput, 0);
+    }
 }
