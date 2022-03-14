c5ef7e6ebe8bb9b0dec2179b659d22c1393c60fa
bexoo <bradyallen67@gmail.com>
2022-03-14 18:42:19 -0400

    Updated name of auto routine

--- RapidCode/src/main/java/frc/robot/commands/GrpAutoFurthestFromHubPickupShoot.java
+++ RapidCode/src/main/java/frc/robot/commands/GrpAutoFurthestFromHubPickupShoot.java
@@ -8,14 +8,14 @@ import frc.robot.subsystems.ShooterLookupTable;
 import frc.robot.subsystems.Vision; 
 import frc.robot.subsystems.BallPath;
 
-public class GrpAutoCompetition extends SequentialCommandGroup {
+public class GrpAutoFurthestFromHubPickupShoot extends SequentialCommandGroup {
     // put any subsystems used in the auto routine in the parameters
     // you don't need any member level variables
-    public GrpAutoCompetition(Drive driveSubsystem, BallPath ballPathSubsystem, Pickup pickupSubsystem, Shooter shooterSubsystem, Vision visionSubsystem, ShooterLookupTable lookupTable) {
+    public GrpAutoFurthestFromHubPickupShoot(Drive driveSubsystem, BallPath ballPathSubsystem, Pickup pickupSubsystem, Shooter shooterSubsystem, Vision visionSubsystem, ShooterLookupTable lookupTable) {
         addCommands(
             new CmdPickupDeploy(pickupSubsystem), 
             new CmdDriveDistance(driveSubsystem, 40.44, .5, true), // Positive is towards the pickup side
-            new CmdDriveRotate(driveSubsystem, 184.3, 0.5, true), // 180 degrees + the angle between the perpendicular and the angle to the target, assuming clockwise is positive
+            new CmdDriveRotate(driveSubsystem, 180 + 4.3, 0.5, true), // 180 degrees + the angle between the perpendicular and the angle to the target, assuming clockwise is positive
             new CmdShooterMoveToPosition(shooterSubsystem, visionSubsystem, lookupTable, 153),
             new CmdShooterShoot(shooterSubsystem, ballPathSubsystem, lookupTable.getVelocityAtDistance(153)),
             new CmdPickupStow(pickupSubsystem)
