c1ae0b7d71822659259fc1479b4dd1b70cab85c0
bexoo <bradyallen67@gmail.com>
2022-03-14 18:15:56 -0400

    Added competition auto routine

--- RapidCode/src/main/java/frc/robot/commands/GrpAutoCompetition.java
+++ RapidCode/src/main/java/frc/robot/commands/GrpAutoCompetition.java
@@ -13,13 +13,12 @@ public class GrpAutoCompetition extends SequentialCommandGroup {
     // you don't need any member level variables
     public GrpAutoCompetition(Drive driveSubsystem, BallPath ballPathSubsystem, Pickup pickupSubsystem, Shooter shooterSubsystem, Vision visionSubsystem, ShooterLookupTable lookupTable) {
         addCommands(
-
             new CmdPickupDeploy(pickupSubsystem), 
             new CmdDriveDistance(driveSubsystem, 40.44, .5, true), 
             new CmdDriveRotate(driveSubsystem, 180, 0.5, true),
             new CmdShooterMoveToPosition(shooterSubsystem, visionSubsystem, lookupTable, 153),
-            new CmdShooterShoot(shooterSubsystem, ballPathSubsystem, lookupTable.getVelocityAtDistance(153))
-
+            new CmdShooterShoot(shooterSubsystem, ballPathSubsystem, lookupTable.getVelocityAtDistance(153)),
+            new CmdPickupStow(pickupSubsystem)
         );
     }
 }
