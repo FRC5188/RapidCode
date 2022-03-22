6b2b6e0a099a1d899da03c838e3340aed17de5df
first5188 <first5188@gmail.com>
2022-03-22 18:10:59 -0400

    Updated 2-ball auto routine

---
+++ RapidCode/src/main/java/frc/robot/commands/CmdShooterAdjustSpeed.java
@@ -0,0 +1,28 @@
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class CmdShooterAdjustSpeed extends CommandBase {
+
+    public CmdShooterAdjustSpeed(double speedAdjustment) {
+
+    }
+
+    @Override
+    public void initialize() {
+    }
+
+    @Override
+    public void execute() {
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
