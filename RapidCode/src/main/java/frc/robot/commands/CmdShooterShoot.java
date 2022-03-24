a993a7ff478b1116c7c0ba8e0449a21b68e82ec7
rhit-edgingza <edgingza@rose-hulman.edu>
2022-03-23 23:11:11 -0400

    Fixed no ball path or accel during shooting

--- RapidCode/src/main/java/frc/robot/commands/CmdShooterShoot.java
+++ RapidCode/src/main/java/frc/robot/commands/CmdShooterShoot.java
@@ -41,19 +41,11 @@ public class CmdShooterShoot extends CommandBase {
 
     @Override
     public void execute() {
-        // if (m_ballPathSubsystem.hasLeftBallPath() && m_ballPathSubsystem.getBallCount() > 1) {
-        //     m_useTimer = true;
-        // }
+        m_ballPathSubsystem.setMotorSpeed(Constants.BALL_PATH_SHOOTING_SPEED);
+        m_shooterSubsystem.setAcceleratorSpeed(0.4);
+        m_shooterSubsystem.setTopFlywheelSpeed(m_velocity);
+        m_shooterSubsystem.setBottomFlywheelSpeed(m_velocity);
 
-        // if (m_useTimer) m_timer--;
-        // else {
-        //     m_ballPathSubsystem.setMotorSpeed(Constants.BALL_PATH_SHOOTING_SPEED);
-        //     m_shooterSubsystem.setAcceleratorSpeed(0.4);
-        //     m_shooterSubsystem.setTopFlywheelSpeed(m_velocity);
-        //     m_shooterSubsystem.setBottomFlywheelSpeed(m_velocity);
-        // }
-
-        // if (m_timer <= 0) m_useTimer = false;
         m_timer--;
     }
 
